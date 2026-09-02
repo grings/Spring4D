c071569078e709ea43d9ad4aed42d9990dbe2c5a
Stefan Glienke <sglienke@dsharp.org>
2026-09-02 11:42:01 +0200

    fixed compile error

--- Source/Data/ObjectDataSet/Spring.Data.ObjectDataSet.pas
+++ Source/Data/ObjectDataSet/Spring.Data.ObjectDataSet.pas
@@ -520,7 +520,7 @@ var
 begin
   Result := TCollections.CreateList<T>;
   for i := 0 to IndexList.Count - 1 do
-    Result.Add(IndexList.Items[i]);
+    Result.Add(T(IndexList.Items[i]));
 end;
 
 function TObjectDataSet.GetRecordCount: Integer;
